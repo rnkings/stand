44e62d5abe584ab802258b4becb6df587a08e1f8
Rose Koenings <rnkings.05@gmail.com>
2016-07-01 13:38:30 -0500

    table almost working

--- lemonadeStand.js
+++ lemonadeStand.js
@@ -3,10 +3,10 @@
 function startLemonadeStand(){
 	console.log(document.weather);
 }
-//Person Class
+//Person Class This is used in generateResults which will determine liklihood of people wanting to buy lemonade.
 function Person(temp, price){
-	var likeihood=Math.random()/price;
-	if(temp>75){
+	var likeihood=Math.random()/price;//var
+	if(temp>75){						
 		likeihood=likeihood*(temp%75);
 	}
 	if(likeihood>.5)
@@ -16,7 +16,7 @@ function Person(temp, price){
 		}
 	}
 
-
+//Profit for the day
 function DayResult(day, peopleArray, cost, price, amountSold){
 	this.day=day;
 	this.peopleArray=peopleArray;
@@ -24,8 +24,8 @@ function DayResult(day, peopleArray, cost, price, amountSold){
 	this.price=price;
 	this.amountSold = amountSold;
 	this.totalProfit=function(){
-		var totalCost = this.price*this.amountSold;
-		var grossProfit = this.cost*this.amountSold;
+		var totalCost = this.price*this.amountSold;//var
+		var grossProfit = this.cost*this.amountSold;//var
 
 		return (grossProfit-totalCost);
 	};
@@ -34,10 +34,10 @@ function DayResult(day, peopleArray, cost, price, amountSold){
 
 //stores user input for #ofdays, #pplWalkedBy, price and cost
 function simulate(){
-	var days = document.getElementById("numberOfDays").value;
-	var passerbys = document.getElementById("peopleWalkedBy").value;
-	var price=document.getElementById("priceCharged").value;
-	var cost=document.getElementById("costOfLemonade").value;
+	var days = document.getElementById("numberOfDays").value;//var
+	var passerbys = document.getElementById("peopleWalkedBy").value;//var
+	var price=document.getElementById("priceCharged").value;//var
+	var cost=document.getElementById("costOfLemonade").value;//var
 	var dayResultArray=[];
 
 	// console.log(days);
@@ -45,12 +45,14 @@ function simulate(){
 	// console.log(price);
 	// console.log(cost);
 	for(var i=0; i<days;i++){
-		var dayResult = generateResults(i, passerbys, price, cost);//calling generate results it creates a new day result
+		console.log(dayResultArray);
+		var dayResult = generateResults(i, passerbys, price, cost);//was var (calling generate results it creates a new day result
 		dayResultArray.push(dayResult);
 	}
+	return dayResultArray;
 }	
 
-
+//determines if people will buy lemonade
 function generateResults(day, passerbys, price, cost){
 	//console.log("=====================");
 	// console.log(day);
@@ -58,10 +60,10 @@ function generateResults(day, passerbys, price, cost){
 	// console.log(price);
 	// console.log(cost);
 	// console.log("=====================");
-	var temp = document.weather.forecast[day].high;
+	var temp = document.weather.forecast[day].high;//var
 		//console.log(temp);
-	var peopleArray=[];	
-	var amountSold = 0;
+	var peopleArray=[];//var
+	var amountSold = [];//var
 	for(var i=0;i<passerbys;i++){
 		var person = new Person(temp, price);
 
@@ -72,27 +74,29 @@ function generateResults(day, passerbys, price, cost){
 		peopleArray.push(person);
 		//console.log(i);
 	}
-	var result= new DayResult(day, peopleArray, cost, price, amountSold);
+	var result= new DayResult(day, peopleArray, cost, price, amountSold);//var
 	return result;
 }
 
 
-function makeOuputTable(elementID){
-		var element=document.getElementById(elementID);
-		var elementHTML = "<table><tr><th>Days</th>" + 
+
+
+function tableCreate(){
+	var dayResultArray = simulate();
+	var element=document.getElementById("displayTable");
+	var elementHTML = "<table><tr><th>Days</th>" + 
 						    "<th>Passerby</th>" + 
-							"<th>Cost</th>"+
+							"<th>Glasses Sold</th>"+
 							"<th>Profit</th></tr>";
-		for (var i = 0; i < this.dayResultArray.length; i++){
-			elementHTML += "<tr><td>" + this.dayResultArray[i][0] + "</td><td>" + this.dayResultArray[i][1] + "</td><td>" + this.dayResultArray[i][2] + "</td>" +
-							"<td><button type='button' onclick='generateResults.DayResult(" + this.dayResultArray[i] + ")'>GET COST</button></td></tr>";
-		}
-		elementHTML += "</table>";
-		element.innerHTML = elementHTML;
+	//tbl.style.width='100%';
+	//tbl.setAttribute('border', '1');
+	for(var i=0; i<dayResultArray.length;i++){
+		elementHTML += "<tr><td>" + (i+1) + "</td><td>" + dayResultArray[i].peopleArray.length + "</td><td>" + dayResultArray[i].amountSold + "</td><td>" + dayResultArray[i].totalProfit() + "</td></tr>";
+
+	}
+	elementHTML += "</table";
+	element.innerHTML=elementHTML;
 }
-
-
-
 
 
 
